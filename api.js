b52de6fd5e485610841ec4d010ee820231bf215a
Tencks <104030650+Tencks@users.noreply.github.com>
2023-10-10 18:32:28 -0300

    Update api.js

--- api.js
+++ api.js
@@ -1,74 +1,20 @@
-
 const apiKey = '62d9b372599449c397e41753beabff98';
 const countryKey = localStorage.getItem('idioma');
+const apiUrl = `https://newsapi.org/v2/top-headlines?country=${countryKey}&category=music&apiKey=${apiKey}`;
 
-const colorTexto =  localStorage.getItem('textColor');
-const colorFondo = localStorage.getItem('fondoColor');
+const xhr = new XMLHttpRequest();
+xhr.open('GET', apiUrl, true);
 
-const token = localStorage.getItem('token');
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4) {
+    if (xhr.status === 200) {
+      const response = JSON.parse(xhr.responseText);
+      console.log(response);
+      // Procesa los datos aquí
+    } else {
+      console.error('Error en la solicitud:', xhr.status, xhr.statusText);
+    }
+  }
+};
 
-function getMusicNews(){
-    const apiUrl = `https://newsapi.org/v2/top-headlines?country=${countryKey}&category=music&apiKey=${apiKey}`;
-    const noticiasContainer = document.getElementById('noticias');
-        
-
-    fetch(apiUrl)
-        .then((response) => response.json())
-        .then ((data) =>{
-            if (data.articles && Array.isArray(data.articles)) {
-                const noticias = data.articles;
-
-                const noticiasCard = document.getElementById('noticiasCard');
-
-
-                noticias.forEach((noticia) => {
-                    const titulo = noticia.title;
-                    const descripcion = noticia.description;
-                    const urlNotice = noticia.url;
-                    const autor = noticia.author;
-                    const Img = noticia.urlToImage;
-
-                    console.log(noticia)
-
-                        // Clonar el elemento card y agregarlo al contenedor
-                        const noticiaDiv = noticiasCard.cloneNode(true);
-
-                    const noticiasTitle = document.querySelector('#noticiasTitle');
-                        noticiasTitle.textContent = titulo;
-                        
-                    const  noticiaImg = document.querySelector('#noticiasImg');
-                        noticiaImg.src = Img;
-
-                    const noticiasDescription = document.querySelector('#noticiasDescription');
-                        noticiasDescription.textContent = descripcion;
-
-                    const noticiasAutor = document.querySelector('#noticiasAutor');
-                        noticiasAutor.textContent = `Autor: ${autor}`;
-                        
-                    const noticiasUrl = document.querySelector('#noticiasUrl');
-                        noticiasUrl.textContent = 'Leer más';
-                        noticiasUrl.href = urlNotice;
-
-                        
-
-                    noticiasContainer.appendChild(noticiaDiv);
-                    
-
-                });
-            } else {
-                console.error('La respuesta del API no tiene artículos válidos');
-            }
-
-
-        })
-        .catch((error) =>{
-            console.log('Fallo la obtención de noticias: ',error)
-        })
-}
-
-
-if (token){
-    document.addEventListener('DOMContentLoaded', getMusicNews)
-}else{
-    console.log('no se ha encontrado un token válido para mostrar las noticias')
-}
+xhr.send();
